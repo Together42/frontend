1cc639acfeaec7dda8d5eaae23a6ab0bd6674133
keinn51 <soogood97@gmail.com>
2022-04-24 01:40:46 +0900

    FEATURE: 로그인에 회원가입 기능 추가

--- src/components/Auth/AuthForm.tsx
+++ src/components/Auth/AuthForm.tsx
@@ -1,23 +1,34 @@
 import React, { useState } from 'react';
 import '@css/Auth/AuthForm.scss';
 
-function AuthForm() {
+interface Props {
+  mode: boolean;
+}
+
+function AuthForm(props: Props) {
+  const { mode } = props;
   const [id, setId] = useState('');
   const [password, setPassword] = useState('');
+  const [passCheck, setPassCheck] = useState('');
 
   const onSubmit = (e: any) => {
     e.preventDefault();
+    // write this code with data!
   };
 
   const onChange = (e: any) => {
     if (e.target.id === 'id') setId(e.target.value);
     else if (e.target.id === 'password') setPassword(e.target.value);
+    else setPassCheck(e.target.value);
   };
 
   return (
     <div className="authForm">
       <form className="authForm--form" onSubmit={onSubmit}>
-        <div>
+        <div className="authForm--forFlex">
+          <div className="authForm--label">
+            <span>아이디</span>
+          </div>
           <input
             className="authForm--input"
             id="id"
@@ -26,7 +37,10 @@ function AuthForm() {
             value={id}
           ></input>
         </div>
-        <div>
+        <div className="authForm--forFlex">
+          <div className="authForm--label">
+            <span>비밀번호</span>
+          </div>
           <input
             className="authForm--input password"
             id="password"
@@ -35,7 +49,21 @@ function AuthForm() {
             value={password}
           ></input>
         </div>
-        <button className="authForm--button">로그인</button>
+        {mode && (
+          <div className="authForm--forFlex">
+            <div className="authForm--label">
+              <span>비번확인</span>
+            </div>
+            <input
+              className="authForm--input passCheck"
+              id="passCheck"
+              placeholder="비밀번호 재입력"
+              onChange={onChange}
+              value={passCheck}
+            ></input>
+          </div>
+        )}
+        <button className="authForm--button">{mode ? '회원가입' : '로그인'}</button>
       </form>
     </div>
   );
--- src/components/Auth/index.tsx
+++ src/components/Auth/index.tsx
@@ -1,19 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from '@utils/Header';
 import Navbar from '@utils/Navbar';
 import AuthForm from './AuthForm';
 import '@css/Auth/Auth.scss';
 
 function Auth() {
+  const [mode, setMode] = useState(false);
+  const onClickMode = () => {
+    setMode((prev) => !prev);
+  };
   return (
     <>
       <Header />
       <Navbar />
       <div className="auth--wrapper">
-        <p className="auth--hello">어서오세요 사서님. 친해질 시간입니다!</p>
-        <AuthForm />
-        <p className="auth--guide">아직 가입을 안 하신 사서님은 여기로!</p>
-        <p className="auth--mode_change">회원가입</p>
+        {mode ? (
+          <>
+            <p className="auth--hello">관리자의 승인 후 로그인이 가능합니다</p>
+            <p className="auth--hello">얼마 걸리지 않을거에요!</p>
+          </>
+        ) : (
+          <p className="auth--hello">어서오세요 사서님, 친해질 시간입니다!</p>
+        )}
+        <AuthForm mode={mode} />
+        <p className="auth--guide">
+          {mode ? '알고 보니 회원가입을 하셨다면?' : '아직 가입을 안 하신 사서님은 여기로!'}
+        </p>
+        <p className="auth--mode_change" onClick={onClickMode}>
+          {mode ? '로그인' : '회원가입'}
+        </p>
       </div>
     </>
   );
