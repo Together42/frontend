9195e8e9ec9236c720f5b6175765a8097cb8691b
keinn51 <soogood97@gmail.com>
2022-04-23 23:36:48 +0900

    FEATURE: header에 usenavigate 추가

--- src/components/utils/Header.tsx
+++ src/components/utils/Header.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import header_image from '@img/header_image.png';
 import '@css/utils/Header.scss';
+import { useNavigate } from 'react-router-dom';
 
 function Header() {
+  const navigate = useNavigate();
+
+  const onClickHeader = () => {
+    navigate('/');
+  };
   return (
     <div className="header--wrapper">
-      <img src={header_image} alt="header_image" className="header--image"></img>
-      <p className="header--title">Together 42</p>
+      <img src={header_image} alt="header_image" className="header--image" onClick={onClickHeader}></img>
+      <p className="header--title" onClick={onClickHeader}>
+        Together 42
+      </p>
     </div>
   );
 }
--- src/components/utils/Navbar.tsx
+++ src/components/utils/Navbar.tsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 function Navbar() {
   return (
     <div className="navbar--wrapper">
-      <Link to={`/`}>신청 및 결과</Link>
+      <Link to={`/`}>신청&결과</Link>
       <Link to={`/review`}>친스타그램</Link>
       <Link to={`/auth`}>로그인</Link>
     </div>
