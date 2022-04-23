4ca14e58107f8a51033fdfdf01a3cef80050ab75
keinn51 <soogood97@gmail.com>
2022-04-23 21:35:22 +0900

    EDIT: main result에 hr 추가

--- src/components/Main/MainResult.tsx
+++ src/components/Main/MainResult.tsx
@@ -2,30 +2,35 @@ import React from 'react';
 import '@css/MainResult.scss';
 
 function MainResult() {
+  const tempArr = ['tmam', 'jwoo', 'sujikim', 'seongyle'];
   return (
     <div className="main--result">
       <p className="main--result--title">친바 결과보기</p>
       <div className="main--result--table">
         <div>
-          <p className="main--result--team_name">team1</p>
-          <p>tmam</p>
-          <p>tmam</p>
-          <p>tmam</p>
-          <p>tmam</p>
+          <p className="main--result--team_name">team one</p>
+          {tempArr.map((e, i) => (
+            <p key={i} className="main--result--intra">
+              {e}
+            </p>
+          ))}
+        </div>
+        <hr className="hrhr"></hr>
+        <div>
+          <p className="main--result--team_name">team two</p>
+          {tempArr.map((e, i) => (
+            <p key={i} className="main--result--intra">
+              {e}
+            </p>
+          ))}
         </div>
         <div>
-          <p className="main--result--team_name">team2</p>
-          <p>tmam</p>
-          <p>tmam</p>
-          <p>tmam</p>
-          <p>tmam</p>
-        </div>
-        <div>
-          <p className="main--result--team_name">team3</p>
-          <p>tmam</p>
-          <p>tmam</p>
-          <p>tmam</p>
-          <p>tmam</p>
+          <p className="main--result--team_name">team three</p>
+          {tempArr.map((e, i) => (
+            <p key={i} className="main--result--intra">
+              {e}
+            </p>
+          ))}
         </div>
       </div>
     </div>
