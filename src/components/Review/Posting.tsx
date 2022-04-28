cb26dd11c8e57fbfae488e550c190e890cb45226
keinn51 <soogood97@gmail.com>
2022-04-28 12:25:31 +0900

    ADD: 리뷰페이지 datail comment 추가

--- src/components/Review/Posting.tsx
+++ src/components/Review/Posting.tsx
@@ -9,12 +9,21 @@ interface Props {
   teamName: string;
   comment: string;
   memberArr: string[];
+  location: string;
   elemNum: number;
 }
 
 function Posting(props: Props) {
-  const { image, teamName, comment, memberArr, elemNum } = props;
+  const { image, teamName, comment, memberArr, elemNum, location } = props;
   const [modalShow, setModalShow] = useState(false);
+  const tempVistComment = [
+    '자주 가는 순대국집이네요ㅎㅎ',
+    '자주 가는 순대국집이네요ㅎㅎ',
+    '자주 가는 순대국집이네요ㅎㅎ',
+    '자주 가는 순대국집이네요ㅎㅎ',
+    '자주 가는 순대국집이네요ㅎㅎ',
+  ];
+  const tempVisName = ['jwoo', 'jwoo', 'jwoo', 'jwoo', 'jwoo'];
 
   const onClickMoreButton = (e) => {
     setModalShow(true);
@@ -28,34 +37,52 @@ function Posting(props: Props) {
     >
       <div className={`review--posting--shownWrapper ${modalShow && 'wrapper-modal_show-in_web'}`}>
         <div className={`review--posting--title ${modalShow && 'title-modal_show-in_web'}`}>
-          <span>{teamName}</span>
+          <div>
+            <span className="review--posting--title--team">{teamName}</span>
+            <span className="review--posting--title--location">{location}</span>
+          </div>
           <div className="review--posting--members">
             {memberArr.map((e, i) => (
               <img src={e} key={i} alt={e} />
             ))}
           </div>
         </div>
+        {modalShow && <div className="review--posting--image--background"></div>}
         <div className={`review--posting--image ${modalShow && 'image-modal_show-in_web'}`}>
           <img src={image} alt={image} />
         </div>
-        <div className="review--posting--comments">
-          <p className="review--posting--maincomment">{comment}</p>
-          <div className="review--posting--subcomment">
-            <p>
-              <span>댓글 3개 모두 보기</span>
-            </p>
-            <p>
-              <span>04.12</span>
-            </p>
+        {modalShow ? (
+          <div className="review--posting--detail_comments">
+            <span className="review--posting--full_comment">{comment}</span>
+            {tempVisName.map((e, i) => (
+              <div>
+                <span className="review--posting--visitor">{e}</span>
+                <span className="review--posting--visitor_comment">{tempVistComment[i]}</span>
+              </div>
+            ))}
           </div>
-        </div>
-        <div
-          className={`review--posting--open_modal ${elemNum % 2 === 0 ? 'position-right' : 'position-left'}`}
-          onClick={onClickMoreButton}
-        >
-          <span>더보기</span>
-          <img src={caret_right} alt={caret_right} />
-        </div>
+        ) : (
+          <>
+            <div className="review--posting--comments">
+              <p className="review--posting--maincomment">{comment}</p>
+              <div className="review--posting--subcomment">
+                <p>
+                  <span>댓글 3개 모두 보기</span>
+                </p>
+                <p>
+                  <span>04.12</span>
+                </p>
+              </div>
+            </div>
+            <div
+              className={`review--posting--open_modal ${elemNum % 2 === 0 ? 'position-right' : 'position-left'}`}
+              onClick={onClickMoreButton}
+            >
+              <span>더보기</span>
+              <img src={caret_right} alt={caret_right} />
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
