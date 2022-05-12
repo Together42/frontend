d64f96b5c47fa7ae765b7ff2ae334fcbaaad3cc5
keinn51 <soogood97@gmail.com>
2022-05-12 23:43:55 +0900

    FIX: 로그인 토큰 클라이언트에서 삭제 되지 않는 오류

--- src/cert/TokenStorage.ts
+++ src/cert/TokenStorage.ts
@@ -1,13 +1,28 @@
 const TOKEN = 'token';
-
-export function saveToken(token: string) {
-  localStorage.setItem(TOKEN, token);
-}
-
-export function getToken() {
-  return localStorage.getItem(TOKEN);
-}
+const expireHour = 2;
 
 export function clearToken() {
   localStorage.clear();
+}
+
+export function saveToken(token: string) {
+  const tokenObj = {
+    value: token,
+    expire: Date.now() + expireHour * 60 * 60 * 1000,
+  };
+  const tokenObjString = JSON.stringify(tokenObj);
+  localStorage.setItem(TOKEN, tokenObjString);
+}
+
+export function getToken() {
+  const tokenObjString = localStorage.getItem(TOKEN);
+  if (!tokenObjString) {
+    return null;
+  }
+  const tokenObj = JSON.parse(tokenObjString);
+  if (Date.now() > tokenObj.expire) {
+    clearToken();
+    return null;
+  }
+  return tokenObj.value;
 }
