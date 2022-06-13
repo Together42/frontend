13daab2eb1295cfa1a27c9ca5e8dbd792631d6f8
keinn51 <soogood97@gmail.com>
2022-06-13 21:50:20 +0900

    EDIT: 웹 호환성 문제에 의해 webp를 jpg로 교체

--- src/globalObj/object/ProfileImageArr.ts
+++ src/globalObj/object/ProfileImageArr.ts
@@ -1,61 +1,61 @@
-import profile1 from '@img/profile-1.webp';
-import profile2 from '@img/profile-2.webp';
-import profile3 from '@img/profile-3.webp';
-import profile4 from '@img/profile-4.webp';
-import profile5 from '@img/profile-5.webp';
-import profile6 from '@img/profile-6.webp';
-import profile7 from '@img/profile-7.webp';
-import profile8 from '@img/profile-8.webp';
-import profile9 from '@img/profile-9.webp';
-import profile10 from '@img/profile-10.webp';
-import profile11 from '@img/profile-11.webp';
-import profile12 from '@img/profile-12.webp';
-import profile13 from '@img/profile-13.webp';
-import profile14 from '@img/profile-14.webp';
-import profile15 from '@img/profile-15.webp';
-import profile16 from '@img/profile-16.webp';
-import profile17 from '@img/profile-17.webp';
-import profile18 from '@img/profile-18.webp';
-import profile19 from '@img/profile-19.webp';
-import profile20 from '@img/profile-20.webp';
-import profile21 from '@img/profile-21.webp';
-import profile22 from '@img/profile-22.webp';
-import profile23 from '@img/profile-23.webp';
-import profile24 from '@img/profile-24.webp';
-import profile25 from '@img/profile-25.webp';
-import profile26 from '@img/profile-26.webp';
-import profile27 from '@img/profile-27.webp';
-import profile28 from '@img/profile-28.webp';
+import profile_1 from '@img/profile-1.jpg';
+import profile_2 from '@img/profile-2.jpg';
+import profile_3 from '@img/profile-3.jpg';
+import profile_4 from '@img/profile-4.jpg';
+import profile_5 from '@img/profile-5.jpg';
+import profile_6 from '@img/profile-6.jpg';
+import profile_7 from '@img/profile-7.jpg';
+import profile_8 from '@img/profile-8.jpg';
+import profile_9 from '@img/profile-9.jpg';
+import profile_10 from '@img/profile-10.jpg';
+import profile_11 from '@img/profile-11.jpg';
+import profile_12 from '@img/profile-12.jpg';
+import profile_13 from '@img/profile-13.jpg';
+import profile_14 from '@img/profile-14.jpg';
+import profile_15 from '@img/profile-15.jpg';
+import profile_16 from '@img/profile-16.jpg';
+import profile_17 from '@img/profile-17.jpg';
+import profile_18 from '@img/profile-18.jpg';
+import profile_19 from '@img/profile-19.jpg';
+import profile_20 from '@img/profile-20.jpg';
+import profile_21 from '@img/profile-21.jpg';
+import profile_22 from '@img/profile-22.jpg';
+import profile_23 from '@img/profile-23.jpg';
+import profile_24 from '@img/profile-24.jpg';
+import profile_25 from '@img/profile-25.jpg';
+import profile_26 from '@img/profile-26.jpg';
+import profile_27 from '@img/profile-27.jpg';
+import profile_28 from '@img/profile-28.jpg';
 
 const tempImageArr = [
-  profile1,
-  profile2,
-  profile3,
-  profile4,
-  profile5,
-  profile6,
-  profile7,
-  profile8,
-  profile9,
-  profile10,
-  profile11,
-  profile12,
-  profile13,
-  profile14,
-  profile15,
-  profile16,
-  profile17,
-  profile18,
-  profile19,
-  profile20,
-  profile21,
-  profile22,
-  profile23,
-  profile24,
-  profile25,
-  profile26,
-  profile27,
-  profile28,
+  profile_1,
+  profile_2,
+  profile_3,
+  profile_4,
+  profile_5,
+  profile_6,
+  profile_7,
+  profile_8,
+  profile_9,
+  profile_10,
+  profile_11,
+  profile_12,
+  profile_13,
+  profile_14,
+  profile_15,
+  profile_16,
+  profile_17,
+  profile_18,
+  profile_19,
+  profile_20,
+  profile_21,
+  profile_22,
+  profile_23,
+  profile_24,
+  profile_25,
+  profile_26,
+  profile_27,
+  profile_28,
 ];
 
 export default tempImageArr;
