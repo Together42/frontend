435b00249e7ebd27abd1f4bc59ee7a9ddeede82e
keinn51 <soogood97@gmail.com>
2022-06-13 20:50:24 +0900

    이틀동안 기존 캐시를 전체 삭제합니다

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
 import loadable from '@loadable/component';
 import '@css/fonts.scss';
 import Header from '@utils/Header';
 import Navbar from '@utils/Navbar';
+import { clearToken } from '@cert/TokenStorage';
 
 const Main = loadable(() => import('@main/index'));
 const Auth = loadable(() => import('@auth/index'));
@@ -12,6 +13,11 @@ const Review = loadable(() => import('@review/index'));
 const Result = loadable(() => import('@result/index'));
 
 const App = () => {
+  // 22.06.15일 이후에 반드시 삭제!
+  useEffect(() => {
+    clearToken();
+  }, []);
+
   return (
     <RecoilRoot>
       <Router basename={process.env.NODE_ENV === 'production' ? 'frontend' : ''}>
