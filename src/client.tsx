ff986c27d8d10e9659d4d8383f14d08f5c00c4d2
keinn51 <soogood97@gmail.com>
2022-06-15 16:52:18 +0900

    FEATURE: recoil selector 만들어 deviceMode에 적용

--- src/client.tsx
+++ src/client.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { RecoilRoot } from 'recoil';
 import App from './app';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(
+  <RecoilRoot>
+    <App />
+  </RecoilRoot>,
+  document.getElementById('app'),
+);
