b1cc9d5c278604323e5b1ab12f910f017d784bac
yenawee <yaena0319@naver.com>
2022-12-11 22:10:59 +0900

    feat : brush image 추가

--- src/components/Timeline/Home.tsx
+++ src/components/Timeline/Home.tsx
@@ -2,11 +2,15 @@ import React from 'react';
 import { Chrono } from 'react-chrono';
 import timelineData from './data';
 import '@css/Timeline/Home.scss';
+import brush from '@img/timeline/paint-brush.png';
 
 export const Home = () => {
   return (
     <div className="timeline--wrapper">
-      <div className="timeline--title">2022 집현전 실록</div>
+      <div className="timeline--title">
+        2022 집현전 실록
+        <img className="timeline--title-img" src={brush} />
+      </div>
       <div style={{ width: '750px', height: '800px' }}>
         <Chrono
           items={timelineData}
