42cb2f2dbf3db2c583e1a016d443fc6030fb349f
yenawee <yaena0319@naver.com>
2022-12-26 22:45:12 +0900

    feat : timeline 이미지 api 요청으로 불러오기

--- src/components/Timeline/Home.tsx
+++ src/components/Timeline/Home.tsx
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Chrono } from 'react-chrono';
 import timelineData from './data';
 import '@css/Timeline/Home.scss';
 import brush from '@img/timeline/paint-brush.png';
+import items from './items';
+import axios from 'axios';
 
 export const Home = () => {
+  const [ImgArr, setImgArr] = useState(null);
+
+  const getImg = () => {
+    axios
+      .get('https://yts.mx/api/v2/list_movies.json?page=1&sort_by=rating')
+      .then((res) => res.data.data.movies)
+      .then((res2) => setImgArr(res2))
+      .catch((err) => console.log(err));
+  };
+
+  useEffect(() => {
+    getImg();
+  }, []);
+
   return (
     <div className="timeline--wrapper">
       <div className="timeline--title">
@@ -13,7 +29,7 @@ export const Home = () => {
       </div>
       <div className="timeline--chrono" style={{}}>
         <Chrono
-          items={timelineData}
+          items={items}
           scrollable={{ scrollbar: true }}
           slideShow
           slideItemDuration={2000}
@@ -27,7 +43,15 @@ export const Home = () => {
             titleColorActive: 'white',
           }}
           fontSizes={{ cardSubtitle: '0.85rem', cardText: '0.8rem', cardTitle: '1rem', title: '1.3rem' }}
-        ></Chrono>
+        >
+          {ImgArr
+            ? ImgArr.map((elem) => (
+                <div>
+                  <img src={elem['medium_cover_image']} alt={elem['medium_cover_image']}></img>
+                </div>
+              ))
+            : null}
+        </Chrono>
       </div>
     </div>
   );
---
+++ src/components/Timeline/items.ts
@@ -0,0 +1,86 @@
+export default [
+  {
+    title: 'DEC. 21 ~ JAN',
+    cardTitle: '신입 사서가 들어왔어요!',
+    cardSubtitle:
+      '5기(+4기) 사서들이 들어왔습니다! 하지만 코로나 이슈로 클러스터가 닫혀서 정기회의는 게더타운으로 진행했어요🥲',
+  },
+  {
+    cardTitle: '라벨지 제작 및 도서 카테고리 분류 작업',
+    cardSubtitle: '도서들 카테고리 분류 작업에 들어갔어요! 도서에 부착할 라벨지를 제작하고 부착 작업을 했어요',
+  },
+  { cardTitle: '열심히 라벨을 부착하는 중', cardSubtitle: '라벨링은 생각보다 손이 많이 가는 작업입니다' },
+  { cardTitle: '굿즈가 도착했습니다', cardSubtitle: '집현전의 예쁜 책갈피와 스티커 굿즈가 왔어요' },
+  {
+    cardTitle: '친바 프로토타입의 서막이 오르다',
+    cardSubtitle:
+      '갑자기 인원이 너무 많아진 집현전! 동아리원들의 친목을 위해 친해지길바라(aka 친바) 가 시작되었습니다👏',
+  },
+  {
+    title: 'FEB',
+    cardTitle: '카테고리 명판까지 붙은 예쁜 집현전의 모습',
+    cardSubtitle: '도서 정리 작업 후 정돈된 예쁜 집현전',
+  },
+  { cardTitle: '친바는 순항중' },
+  { cardTitle: '친바는 순항중2' },
+  { title: 'MAR', cardTitle: '6기 오티', cardSubtitle: '6기 오리엔테이션에 참석했습니다 !' },
+  { cardTitle: '신입 사서가 들어왔어요', cardSubtitle: '6기와 함께 하는 친바!' },
+  {
+    title: 'APR',
+    cardTitle: '1회차 사서의 날',
+    cardSubtitle: '4/5 서울숲에서 체육대회를 진행했어요! 행사 진행하신 tkim님과 오락부 여러분 수고하셨습니다!!',
+  },
+  {
+    cardTitle: '단체샷!',
+    cardSubtitle: '이 날 날씨가 너무 좋았는데 다들 기억나시나요~?',
+  },
+  { title: 'MAY', cardTitle: '집현전 이모티콘 등록!', cardSubtitle: 'jwoo님께 박수 짝짝' },
+  { cardTitle: '친바 사이트가 배포되었습니다❤️' },
+  {
+    title: 'JUN',
+    cardTitle: '2회차 사서의 날',
+    cardSubtitle: '6/20 이대 파티룸에서 진행된 2차 사서의 날! 진행하신 tkim, hyojekim 수고하셨습니다!!',
+  },
+  { cardTitle: '친스타그램이 생겼어요🎵', cardSubtitle: '우리 사진, 동영상들이 추억이 되게 기록으로 남겨봐요' },
+  { cardTitle: '수요지식회의 서막이 오르다', cardSubtitle: 'jaekim의 지라 특강! 두둥' },
+  { title: 'JUL', cardTitle: '7기 오티에 참여했습니다', cardSubtitle: '신입 사서분들을 환영합니다❣️' },
+  {
+    cardTitle: '📚 집현전 웹사이트 오픈 📚',
+    cardSubtitle: '에어테이블에서 벗어나 집현전 웹사이트가 오픈을 했습니다. 개발팀분들 정말 수고 많으셨습니다!!',
+  },
+  { cardTitle: '칭호가 생겼습니다', cardSubtitle: 'Developer of Jiphyeonjeon, Librarian of Jiphyeonjeon' },
+  { cardTitle: '희망 도서가 왔어요', cardSubtitle: '상반기 신청을 받았던 300여권의 좋은 책들이 도착했습니다!! ' },
+  {
+    title: 'AUG',
+    cardTitle: '3회차 사서의 날',
+    cardSubtitle:
+      '8/20 신촌 파티룸에서 3회차 사서의 날 진행했습니다. 진행에 힘써주신 jaekim, kyungsle 수고하셨습니다!! ',
+  },
+  {
+    cardTitle: '책장 broken 이슈 발생....😿',
+    cardSubtitle:
+      '새로 온 책들 정리 작업 중 책장 유리가 파손되는 사건이 발생했습니다. 아쉽게도 빨리 고쳐지지 않아 책들을 장기간 바닥에 두고 운영을 했답니다..  ',
+  },
+  {
+    title: 'SEP~OCT',
+    cardTitle: '[closed]',
+    cardSubtitle:
+      '책장 이슈로 플리마켓이 되어버린 집현전.... 앗... 8기 피신으로 집현전이 1달 반동안 닫히게 되었습니다. 11월에 다시 만나요!',
+  },
+  {
+    title: 'NOV',
+    cardTitle: '8기 오티 홍보!',
+    cardSubtitle: '비실용적 독서 모임에서 집현전 홍보도 겸해주셨답니다 감사합니다👍',
+  },
+  {
+    title: 'DEC',
+    cardTitle: '임시 집현전 운영',
+    cardSubtitle:
+      '집현전 공간이 2층으로 이동합니다. 그동안 좋은 추억 많이 쌓였었던 기존 집현전 공간이 아쉽지만 2층에 예쁘게 공간이 마련되었으면 좋겠습니다. 공간 마련되기 전까지 임시 운영 진행중~',
+  },
+  { cardTitle: '2022 INNO-CON', cardSubtitle: '우수동아리로 상을 받게된 집현전! 이노콘에서 전시 부스도 진행합니다' },
+  {
+    cardTitle: '리뷰와 좋아요',
+    cardSubtitle: '집현전 웹사이트에 생긴 리뷰✏️ 와 좋아요❤️ 개발팀 여러분 고생하셨습니다',
+  },
+];
