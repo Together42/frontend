06871cf08e8e3d490af2800235eab05ea33779bc
yenawee <yaena0319@naver.com>
2023-01-16 15:06:15 +0900

    feat : 사서 이름, 안되는 날짜 받아서 보내기

--- src/components/Rotation/new.tsx
+++ src/components/Rotation/new.tsx
@@ -1,32 +1,70 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { getAuth } from '@cert/AuthStorage';
-import '@css/Rotation/Rotation.scss';
+import '@css/Rotation/New_Rotation.scss';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
-import { SelectedDates } from './new_calendar';
 
 export const NewRotate = () => {
-  const date = new Date();
-  const year = date.getFullYear();
-  const month = date.getMonth() + 1;
+  const currentDate = new Date();
+  const year = currentDate.getFullYear();
+  const month = currentDate.getMonth() + 1;
   const intraId = getAuth() ? getAuth().id : null;
   const [value, onChange] = useState(new Date());
   const [unavailableDates, setUnavailableDates] = useState([]);
+  const [openSelectModal, setOpenSelectModal] = useState(false);
+
+  const onClickDay = (value) => {
+    const date = value.getDate();
+    if (unavailableDates.indexOf(date) > -1) {
+      setUnavailableDates(unavailableDates.filter((e) => e != date));
+    } else {
+      setUnavailableDates([...unavailableDates, date]);
+    }
+  };
+
+  const onClickSetDateModal = () => {
+    setOpenSelectModal(!openSelectModal);
+  };
+
+  const resetDates = () => {
+    setUnavailableDates([]);
+  };
 
   return (
     <>
       <div className="rotation--wrapper">
-        <div>
-          {intraId} 님, {year} {month}월 사서 로테이션에 참여하시나용 ?
+        <div className="rotation--title">
+          {intraId} 님, {year} {month}월 사서 로테이션에 참여하시나요 ?
         </div>
-        <button>yes!</button>
-        <div>참여가 어려운 날짜를 선택해주세요 ! 해당 날짜를 고려해서 랜덤 매칭이 이루어집니다 </div>
-        <div>
-          <Calendar onChange={onChange} value={value}></Calendar>
+        <div className="rotation--button">
+          <div>
+            <button onClick={onClickSetDateModal}>Yes!</button>
+          </div>
+          <div>
+            <button>신청 취소</button>
+          </div>
         </div>
-        <div>
-          <SelectedDates value={value} unavailableDates={unavailableDates} setUnavailableDates={setUnavailableDates} />
-        </div>
+        {openSelectModal ? (
+          <div className="rotation--selectDates">
+            <div className="rotation-selectDates-title">
+              <p>참여가 어려운 날짜를 선택해주세요 !</p>
+              <p>해당 날짜를 고려해서 랜덤 매칭이 이루어집니다</p>
+            </div>
+            <div>
+              <Calendar onClickDay={(value) => onClickDay(value)} value={value}></Calendar>
+            </div>
+            <div className="rotation--viewSelectDates">
+              <div className="rotation-viewSelectDates-title">선택한 날짜</div>
+              <div className="rotation--selectDates-box">
+                {unavailableDates.map((e) => (
+                  <span>{e} </span>
+                ))}
+              </div>
+              <button onClick={resetDates}>reset</button>
+            </div>
+            <button>선택 완료!</button>
+          </div>
+        ) : null}
       </div>
     </>
   );
--- src/components/Rotation/new_calendar.tsx
+++ src/components/Rotation/new_calendar.tsx
@@ -2,12 +2,13 @@ import React, { useState, useEffect, useRef } from 'react';
 
 export const SelectedDates = (props) => {
   const mounted = useRef(false);
+  var arr;
 
   const getUnavailableDates = (value) => {
     const date = value.getDate();
-    if (props.unavailableDates.find(date)) {
-      props.unavailableDates.reduce(date);
-      props.setUnavailableDates(props.unavailableDates);
+    if (props.unavailableDates.indexOf(date) > -1) {
+      arr = props.unavailableDates.filter((e) => e != date);
+      props.setUnavailableDates(arr);
     } else {
       props.unavailableDates.push(date);
       props.setUnavailableDates(props.unavailableDates);
@@ -23,10 +24,11 @@ export const SelectedDates = (props) => {
   }, [props.value]);
 
   return (
-    <div>
-      {props.unavailableDates.map((e) => (
-        <span>{e}</span>
-      ))}
-    </div>
+    <>
+      {props.unavailableDates.length == 0 && <span>선택된 날짜가 없습니다</span>}
+      {props.unavailableDates.map((e) => {
+        return <span>{e}</span>;
+      })}
+    </>
   );
 };
