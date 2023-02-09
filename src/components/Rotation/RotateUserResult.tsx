9d2dfb04441c34a2ab72f25782386e3b0cefbf50
yenawee <yaena0319@naver.com>
2023-02-09 19:07:57 +0900

    feat : button css

--- src/components/Rotation/RotateUserResult.tsx
+++ src/components/Rotation/RotateUserResult.tsx
@@ -9,7 +9,7 @@ export const RotateUserResult = (props) => {
           📚 {props.intraId} 님의 {props.year} {props.month} 월의 사서 로테 참여일입니다 📚
         </div>
         <div className="rotation--resultbox">
-          {props.arr.empty ? <div>참여일 정보가 없습니다</div> : props.arr.map((e) => <div key={e}>{e}</div>)}
+          {props.arr.length == 0 ? <div>참여일 정보가 없습니다</div> : props.arr.map((e) => <div key={e}>{e}</div>)}
         </div>
         <div className="rotation--result-text">
           <p>사서 활동에 감사드립니다 👏</p>
