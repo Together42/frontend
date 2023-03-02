f99bc0db574b25e376296228f7c8942e33cf3b37
jhj9109 <jhj91_09@naver.com>
2023-03-02 19:20:02 +0900

    refactor: 잘게 함수 쪼개기 & 함수 파일간 이동

--- src/components/Rotation/Rotation.tsx
+++ src/components/Rotation/Rotation.tsx
@@ -9,76 +9,95 @@ import { useSWRConfig } from 'swr';
 import 'react-calendar/dist/Calendar.css';
 import '@css/Rotation/New_Rotation.scss';
 import {
-  createInitialObject,
-  createWeekdays,
-  getActiveStartDate,
+  createWeekdaysObject,
   getKoreaDate,
-  rules,
-  setLimitMaxDate,
-  setLimitMinDate,
-  setTileDisabled,
+  getFirstDateOfMonth,
+  getLastDateOfMonth,
+  getDaysInMonth,
+  getFirstDayOfMonth,
+  isWeekend,
 } from './rotaion_utils';
 
 const DEFAULT_CALENDAR_TYPE = 'US';
 
-export const getWeekNumber = (dateFrom = new Date()) => {
-  // 해당 날짜 (일)
-  const currentDate = dateFrom.getDate();
+/**
+ * 사서 로테이션 신청은 특정 기간에 다음달에 대한것
+ * - import 해온 util 함수들을 활용하여
+ * - 입력으로 받은 date에 대해서 해당 월이 아닌 다음달에 대한 정보를 얻는다.
+ */
+const getFirstDayOfNextMonth = (date: Date) => getFirstDayOfMonth(date, 1);
+const getDaysInNextMonth = (date: Date) => getDaysInMonth(date, 1);
+const getFirstDateOfNextMonth = (date: Date) => getFirstDateOfMonth(date, 1);
+const getLastDateOfNextMonth = (date: Date) => getLastDateOfMonth(date, 1);
 
-  // 이번 달 1일로 지정
-  const startOfMonth = new Date(dateFrom.setDate(1));
+/**
+ * 입력으로 받은 현재 시간 정보가 담긴 curr에 대해서 필요한 동작을 수행한다.
+ * 사서 로테이션 신청은 특정 기간에 다음달에 대한것이여서 NextMonth에 대한 함수들을 사용한다.
+ * - createInitialObject: 평일에 대해 date값을 키로, 값은 false로 초기화된 오브젝트 생성
+ * - getActiveStartDate: 달력 시작날짜를 다음달 1일로
+ * - setLimit...: 달력을 클릭할 수 있는 날짜를 다음달 1일 ~ 말일 까지
+ */
+const createInitialObject = (curr: Date) =>
+  createWeekdaysObject(getFirstDayOfNextMonth(curr), getDaysInNextMonth(curr));
+const getActiveStartDate = (curr: Date) => getFirstDateOfNextMonth(curr);
+const setLimitMinDate = (curr: Date) => getFirstDateOfNextMonth(curr);
+const setLimitMaxDate = (curr: Date) => getLastDateOfNextMonth(curr);
 
-  // 이번 달 1일이 무슨 요일인지 확인
-  const weekDay = startOfMonth.getDay(); // 0: Sun ~ 6: Sat
-
-  // ((요일 - 1) + 해당 날짜) / 7일로 나누기 = N 주차
-  return Math.floor((weekDay - 1 + currentDate) / 7) + 1;
+/**
+ * 달력의 클릭 disable 여부 결정하는 함수
+ * - 1일 ~ 말일 제한은 minDate && maxDate 속성을 통해 이루어짐
+ * - 현재는 주말의 경우만 룰로서 활용중
+ */
+const setTileDisabled =
+  (fns: Function[]) =>
+  ({ date, view }) =>
+    fns.some((fn) => fn({ date, view }));
+const rules = {
+  weekdayOnly: ({ date, _view }: { date: Date; _view: any }) => isWeekend(date),
 };
 
-// console.log("test");
-// console.log(createWeekdays(0, 31));
+/**
+ * 전체 평일에 대해 선택여부 정보를 가진 record 오브젝트에서
+ * 선택된 날짜들로만 이루어진 number[] 추출
+ */
+const createUnavailableDates = (record) =>
+  Object.entries(record)
+    .filter(([_, selected]) => selected)
+    .map(([date_key, _]) => parseInt(date_key));
 
 export const Rotate = () => {
-  // const currentDate = new Date();
-  const currentDate = getKoreaDate();
-  // const initialRecord = createInitialObject(currentDate);
-  // console.log(initialRecord);
+  const currentDate = new Date();
+  // const currentDate = getKoreaDate(); //이것이 필요할까? 아직 잘 모르겠다.
+  const initialRecord = createInitialObject(currentDate);
   const year = currentDate.getFullYear();
   const month = ((currentDate.getMonth() + 1) % 12) + 1;
   const intraId = getAuth()?.id ?? null;
   const isRotationApplicationPeriod = true;
   const [value, onChange] = useState<null | Date>(null);
-  const [record, setRecord] = useState({});
-  // const [record, setRecord] = useState({ ...initialRecord });
+  const [record, setRecord] = useState({ ...initialRecord });
   const [unavailableDates, setUnavailableDates] = useState<number[]>([]);
   const [openSelectModal, setOpenSelectModal] = useState(false);
 
   const { mutate } = useSWRConfig();
 
   useEffect(() => {
-    if (value) {
-      const d = value.getDate();
+    const d = value?.getDate();
+    if (!!d && d in initialRecord) {
+      // 초기값 && 애초에 넘어오면 안 되는 날짜 정보가 넘어오는것 방지
       setRecord((prev) => ({ ...prev, [d]: !prev[d] })); // 해당키에 하는 value만 반전
     }
-  }, [value]);
+  }, [value, initialRecord]);
 
   useEffect(() => {
-    // console.log(record);
-    setUnavailableDates(
-      Object.entries(record).reduce(
-        (prev, [date_key, selected]) => (selected ? [...prev, parseInt(date_key)] : prev),
-        [],
-      ),
-    );
+    setUnavailableDates(createUnavailableDates(record));
   }, [record]);
 
   const onClickSetDateModal = () => {
-    setOpenSelectModal(!openSelectModal);
+    setOpenSelectModal((prev) => !prev);
   };
 
   const resetDates = () => {
-    // setUnavailableDates({ ...initialRecord });
-    setRecord({});
+    setRecord({ ...initialRecord });
   };
 
   const onClickPostEvent = () => {
@@ -163,7 +182,7 @@ export const Rotate = () => {
                 activeStartDate={getActiveStartDate(currentDate)}
                 minDate={setLimitMinDate(currentDate)}
                 maxDate={setLimitMaxDate(currentDate)}
-                tileDisabled={setTileDisabled([rules.isWeekend])}
+                tileDisabled={setTileDisabled([rules.weekdayOnly])}
                 value={value}
                 onClickDay={onChange}
               ></Calendar>
--- src/components/Rotation/rotaion_utils.ts
+++ src/components/Rotation/rotaion_utils.ts
@@ -1,3 +1,8 @@
+/**
+ * rotation_utils.ts
+ * - 이곳의 함수들은 주로 로테이션 관련 기능에 필요한 Date관련 핸들링에 필요한 함수들이다.
+ * - 캘린더에서 Date객체를 사용하는 경우가 종종 있어서, 입력을 Date객체를 받도록 했다.
+ */
 export const MILLISEC_IN_SEC = 1000;
 export const SEC_IN_MIN = 60;
 export const MIN_IN_HOUR = 60;
@@ -5,7 +10,7 @@ export const MILLISEC_IN_MIN = SEC_IN_MIN * MILLISEC_IN_SEC;
 export const MILLISEC_IN_HOUR = MIN_IN_HOUR * MILLISEC_IN_MIN;
 export const KST_OFFSET = 9;
 
-export const getKoreaDate = (date: Date = new Date()) => {
+export const getKoreaDate = (date = new Date()) => {
   const utc = date.getTime() + date.getTimezoneOffset() * MILLISEC_IN_MIN;
   const KR_TIME_DIFF = KST_OFFSET * MILLISEC_IN_HOUR;
   const koreaDate = new Date(utc + KR_TIME_DIFF);
@@ -13,26 +18,36 @@ export const getKoreaDate = (date: Date = new Date()) => {
 };
 export const isWeekend = (day: Date | number) =>
   typeof day === 'number' ? (day % 7) % 6 === 0 : day.getDay() % 6 === 0;
-export const setLimitMinDate = (date: Date) => new Date(date.getFullYear(), date.getMonth() + 1, 1);
-export const setLimitMaxDate = (date: Date) => new Date(date.getFullYear(), date.getMonth() + 2, 0);
 
-export const setTileDisabled =
-  (fns: Function[]) =>
-  ({ date, view }) =>
-    fns.some((fn) => fn({ date, view }));
-export const rules = {
-  isWeekend: ({ date, _view }: { date: Date; _view: any }) => isWeekend(date.getDay()),
+/**
+ * get...DateOfMonth: 특정달의 첫날 || 마지막날 리턴
+ * getDaysInMonth: 특정달의 일수 리턴 (e.g. 1월: 31일, 2월은 28일(2024년도엔 29일))
+ * getFirstDayOfMonth: 특정달의 첫날의 day값 리턴 (e.g. 2023년 3월 => 3(수요일을 의미))
+ * createWeekdays...: 특정달의 평일로만 이루어진 배열 || 오브젝트 생성하여 리턴.
+ */
+export const getFirstDateOfMonth = (date: Date, offsetMonth = 0) =>
+  new Date(date.getFullYear(), date.getMonth() + offsetMonth, 1);
+export const getLastDateOfMonth = (date: Date, offsetMonth = 0) =>
+  new Date(date.getFullYear(), date.getMonth() + 1 + offsetMonth, 0);
+
+export const getDaysInMonth = (date: Date, offsetMonth = 0) => getLastDateOfMonth(date, offsetMonth).getDate();
+export const getFirstDayOfMonth = (date: Date, offsetMonth = 0) => getFirstDateOfMonth(date, offsetMonth).getDay();
+
+export const createWeekdaysArray = (firstDayOfMonth: number, daysInMonth: number) =>
+  Array.from({ length: daysInMonth }, (_, idx) => idx + 1).filter((date) => !isWeekend(firstDayOfMonth + date - 1));
+export const createWeekdaysObject = (firstDayOfMonth: number, daysInMonth: number, initialValue = false) =>
+  Object.fromEntries(createWeekdaysArray(firstDayOfMonth, daysInMonth).map((date) => [date, initialValue]));
+
+export const getWeekNumber = (dateFrom = new Date()) => {
+  // 해당 날짜 (일)
+  const currentDate = dateFrom.getDate();
+
+  // 이번 달 1일로 지정
+  const startOfMonth = new Date(dateFrom.setDate(1));
+
+  // 이번 달 1일이 무슨 요일인지 확인
+  const weekDay = startOfMonth.getDay(); // 0: Sun ~ 6: Sat
+
+  // ((요일 - 1) + 해당 날짜) / 7일로 나누기 = N 주차
+  return Math.floor((weekDay - 1 + currentDate) / 7) + 1;
 };
-export const getDaysInMonth = (date: Date, offsetMonth = 0) =>
-  new Date(date.getFullYear(), date.getMonth() + 1 + offsetMonth, 0).getDate();
-export const getDaysInNextMonth = (date: Date) => getDaysInMonth(date, 1);
-export const getFirstDayOfMonth = (date: Date, offsetMonth = 0) =>
-  new Date(date.getFullYear(), date.getMonth() + 1 + offsetMonth, 0).getDay();
-export const getFirstDayOfNextMonth = (date: Date) => getFirstDayOfMonth(date, 1);
-export const getActiveStartDate = (date: Date) => new Date(date.getFullYear(), date.getMonth() + 1, 1);
-export const createWeekdays = (firstDayOfMonth: number, daysInMonth: number) =>
-  new Array(daysInMonth)
-    .fill(null)
-    .reduce((prev, _el, idx) => (isWeekend(firstDayOfMonth + (idx % 7)) ? prev : { ...prev, [idx + 1]: false }), {});
-export const createInitialObject = (date: Date) =>
-  createWeekdays(getFirstDayOfNextMonth(date), getDaysInNextMonth(date));
