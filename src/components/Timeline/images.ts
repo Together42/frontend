d3662eeab88cfb826d96bf03c2ab12a3088c9ad3
Jiwon-Woo <jwoo@student.42seoul.kr>
2023-11-22 13:53:30 +0900

    Update: timeline api 사용 대신 프론트 코드에 이미지 주소 저장해두기
    #121

---
+++ src/components/Timeline/images.ts
@@ -0,0 +1,86 @@
+export default [
+  {
+    url: `${process.env.IMAGE_SERVER_ADR}/timeline/1672679659836_01_gather.png`,
+  },
+  {
+    url: `${process.env.IMAGE_SERVER_ADR}/timeline/1672679671548_02_category.jpg`,
+  },
+  {
+    url: `${process.env.IMAGE_SERVER_ADR}/timeline/1672679697911_03_labeling.jpg`,
+  },
+  {
+    url: `${process.env.IMAGE_SERVER_ADR}/timeline/1672679742370_04_goods.jpg`,
+  },
+  {
+    url: `${process.env.IMAGE_SERVER_ADR}/timeline/1672679749222_05_proto.png`,
+  },
+  {
+    url: `${process.env.IMAGE_SERVER_ADR}/timeline/1672679755169_06_tidy.jpg`,
+  },
+  {
+    url: `${process.env.IMAGE_SERVER_ADR}/timeline/1672679761120_07_chinba1.jpg`,
+  },
+  {
+    url: `${process.env.IMAGE_SERVER_ADR}/timeline/1672679766734_08_chinba2.jpg`,
+  },
+  {
+    url: `${process.env.IMAGE_SERVER_ADR}/timeline/1672679772445_09_sixot.jpg`,
+  },
+  {
+    url: `${process.env.IMAGE_SERVER_ADR}/timeline/1672679777759_10_sixchinba.jpg`,
+  },
+  {
+    url: `${process.env.IMAGE_SERVER_ADR}/timeline/1672679783239_11_full.jpg`,
+  },
+  {
+    url: `${process.env.IMAGE_SERVER_ADR}/timeline/1672679788490_12_all.jpg`,
+  },
+  {
+    url: `${process.env.IMAGE_SERVER_ADR}/timeline/1672679794687_13_emoji.jpg`,
+  },
+  {
+    url: `${process.env.IMAGE_SERVER_ADR}/timeline/1672679800445_14_chinbasite.png`,
+  },
+  {
+    url: `${process.env.IMAGE_SERVER_ADR}/timeline/1672679805503_15_second.jpg`,
+  },
+  {
+    url: `${process.env.IMAGE_SERVER_ADR}/timeline/1672679810072_16_chinsta.png`,
+  },
+  {
+    url: `${process.env.IMAGE_SERVER_ADR}/timeline/1672679814983_17_sooyo.png`,
+  },
+  {
+    url: `${process.env.IMAGE_SERVER_ADR}/timeline/1672679819752_18_sevenot.jpg`,
+  },
+  {
+    url: `${process.env.IMAGE_SERVER_ADR}/timeline/1672679824314_19_jip.png`,
+  },
+  {
+    url: `${process.env.IMAGE_SERVER_ADR}/timeline/1672679829472_20_achievement.png`,
+  },
+  {
+    url: `${process.env.IMAGE_SERVER_ADR}/timeline/1672679833666_21_newbook.jpg`,
+  },
+  {
+    url: `${process.env.IMAGE_SERVER_ADR}/timeline/1672679837852_22_third.jpg`,
+  },
+  {
+    url: `${process.env.IMAGE_SERVER_ADR}/timeline/1672679842557_23_broken.jpg`,
+  },
+  {
+    url: `${process.env.IMAGE_SERVER_ADR}/timeline/1672679847660_24_white.jpg`,
+  },
+  {
+    url: `${process.env.IMAGE_SERVER_ADR}/timeline/1672679852502_25_eightot.jpg`,
+  },
+  {
+    url: `${process.env.IMAGE_SERVER_ADR}/timeline/1672679858083_26_imsi.jpg`,
+  },
+  {
+    url: `${process.env.IMAGE_SERVER_ADR}/timeline/1672679861787_27_inno.jpg`,
+  },
+  {
+    url: `${process.env.IMAGE_SERVER_ADR}/timeline/1672679865836_28_review.jpg`,
+  },
+];
