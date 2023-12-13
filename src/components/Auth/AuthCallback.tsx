eb6f449eacc289a03280d515e168bba946a2a152
seo-wo <4nyth4me@gmail.com>
2023-12-13 17:16:32 +0900

    feat: implement login callback page

--- src/components/Auth/AuthCallback.tsx
+++ src/components/Auth/AuthCallback.tsx
@@ -1,17 +1,18 @@
 import React, { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import { saveToken } from '@cert/TokenStorage';
+import { useNavigate } from 'react-router-dom';
 
 const AuthCallback = () => {
   const location = useLocation();
   const searchParams = new URLSearchParams(location.search);
   const token = searchParams.get('token');
-  
+  const navigate = useNavigate();
 
   useEffect(() => {
     console.log('LoginSucess', token);
     saveToken(token);
-
+    navigate('/');
   }, []);
 
   return <></>;
