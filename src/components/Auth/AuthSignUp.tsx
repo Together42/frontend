1b2d881173a7ad8f1fd1e039879a2c534c991fab
seo-wo <4nyth4me@gmail.com>
2023-12-13 17:14:42 +0900

    feat: implement signup page
    wip: have to edit scss

--- src/components/Auth/AuthSignUp.tsx
+++ src/components/Auth/AuthSignUp.tsx
@@ -1,45 +1,58 @@
-import React, { useEffect } from 'react';
-import axios from 'axios';
-
-const api = axios.create({
-  baseURL: 'http://localhost:4242',
-  withCredentials: true,
-});
+import React from 'react';
+import '@css/Auth/Auth.scss';
+import { useNavigate } from 'react-router';
+import { useSetRecoilState, useRecoilValue } from 'recoil';
+import errorAlert from '@globalObj/function/errorAlert';
+import apiClient from '@service/apiClient';
+import { saveToken } from '@cert/TokenStorage';
+import ProfileChangeModalShow from '@recoil/ProfileChangeModalShow';
+import SignUpProfileState from '@recoil/SignUpProfileState';
+import ProfileModal from '@auth/ProfileModal';
 
 const AuthSignUp = () => {
-  const handleClick = () => {
-    console.log('click');
-    api.get('user/test').then((res) => {
-      console.log(res);
-      window.close();
-    }).catch((err) => {
-      console.log(err);
-    });
-  };
+  const navigate = useNavigate();
+  const setOpenProfileModal = useSetRecoilState(ProfileChangeModalShow);
+  const openProfileModal = useRecoilValue(ProfileChangeModalShow);
+  const profileImage = useRecoilValue(SignUpProfileState);
 
-  const handleSubmit = (e) => {
+  const handleSubmit = (e: any) => {
     e.preventDefault();
     console.log('submit', e.target[0].value);
     console.log('submit', e.target[1].value);
-    api.post('/auth/signup', {
-      nickname: e.target[0].value,
-      slackId: e.target[1].value,
-    }).then((res) => {
-      console.log('success', res);
-    }
-    ).catch((err) => {
-      console.log(err);
-    });
+    apiClient
+      .post('/auth/signup', {
+        nickname: e.target[0].value,
+        slackId: e.target[1].value,
+        imageUrl: profileImage,
+      })
+      .then((res) => {
+        console.log('success', res.data);
+        saveToken(res.data.access_token);
+        navigate('/');
+      })
+      .catch((err) => {
+        errorAlert(err);
+      });
   };
+
+  const onClickOpenProfile = () => {
+    setOpenProfileModal((prev) => !prev);
+  };
+
   return (
     <div>
-      <h1>signup page</h1>
+      <div className={`auth--wrapper ${'signup'} `}>
+        <div className="auth--profile">
+          <img src={profileImage} alt="profile" />
+          <p onClick={onClickOpenProfile}>프로필 변경</p>
+        </div>
+        {openProfileModal && <ProfileModal />}
+      </div>
       <form onSubmit={handleSubmit}>
         <input type="text" placeholder="nick name" maxLength={10} />
         <input type="text" placeholder="slack id" maxLength={20} />
-        <button>제출</button>
+        <button>Sign Up</button>
       </form>
-      <button onClick={handleClick}>test</button>
     </div>
   );
 };
