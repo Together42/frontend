0276de125a6984d400e9fd894a3b2d89ea8c4965
seo-wo <4nyth4me@gmail.com>
2023-12-13 17:08:07 +0900

    feat: use custom axios for api

---
+++ src/service/apiClient.tsx
@@ -0,0 +1,24 @@
+import axios, { AxiosRequestConfig } from 'axios';
+import { getToken } from '@cert/TokenStorage';
+import getAddress from '@globalObj/function/getAddress';
+
+const apiClient = axios.create({
+  baseURL: getAddress(),
+  withCredentials: true,
+});
+
+apiClient.interceptors.request.use(
+  (config: AxiosRequestConfig) => {
+    const token = getToken();
+    if (token) {
+      config.headers['Authorization'] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error: Error) => {
+    console.log('error : ', error);
+    Promise.reject(error);
+  },
+);
+
+export default apiClient;
