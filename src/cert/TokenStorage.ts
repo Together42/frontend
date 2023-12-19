ca1966f88468c65f4490278cd9c58fe857a258e7
jhj9109 <jhj91_09@naver.com>
2023-12-19 17:22:46 +0900

    Fix 로컬스토리지 관리 로직 수정
    상수 관리
    - expireDays 5일 삭제: JwtToken에 대한 expire는 이미 payload에 포함되어 있음
    - 두 매직넘버를 상수로 표현
      - JWT_PART_LENGTH : JWT 토큰은 헤더 | 페이로드 | 시그니처 3부분
      - MS_PER_SEC : 1초 = 1000밀리초
    
    Jwt 오브젝트가 아닌 토큰(string)만 보관
    - expire가 기능적으로 중복되어 있음
    - 토큰(string)만 저장토록 수정
    - saveToekn & getToken 함수 인자나 반환형태가 변하지 않아 기존 코드와 호환 가능
    
    saveToken()
    - 로컬스토리지에 저장하는 형태를 string으로 변경
    
    clearToken()
    - 로컬스토리지 clear아닌 removeItem으로 수정
    
    getToken()
    - 기존과 동일하게 동작
    - 유효하지 않은 토큰은 로컬스토리지에서 삭제 & null 리턴
    - 유효한 토큰만 string 리턴

--- src/cert/TokenStorage.ts
+++ src/cert/TokenStorage.ts
@@ -1,28 +1,78 @@
 const TOKEN = 'token';
-const expireDays = 5;
+
+const JWT_PART_LENGTH = 3;
+const MS_PER_SEC = 1000;
+
+enum JWTPart {
+  Header,
+  Payload,
+  Signature,
+}
+export interface JWTPayload {
+  id: string;
+  isAdmin: number; // 또는 boolean, 상황에 따라 적절한 타입을 선택하세요.
+  iat: number;
+  exp: number;
+}
+
+export function getJWTPart(token: string, part: JWTPart): string {
+  const parts = token.split('.');
+  if (parts.length !== JWT_PART_LENGTH) {
+    throw new Error('Invalid JWT token format.');
+  }
+  return parts[part];
+}
+
+export function isValidJWTPayload(object: any): object is JWTPayload {
+  return (
+    object !== null &&
+    typeof object === 'object' &&
+    'id' in object &&
+    typeof object.id === 'string' &&
+    'isAdmin' in object &&
+    typeof object.isAdmin === 'number' &&
+    'iat' in object &&
+    typeof object.iat === 'number' &&
+    'exp' in object &&
+    typeof object.exp === 'number'
+  );
+}
+
+export function parseJWTPayload(token: string): JWTPayload {
+  try {
+    const payload = JSON.parse(getJWTPart(token, JWTPart.Payload));
+    if (!isValidJWTPayload(payload)) {
+      throw new Error('Invalid JWTPayload');
+    }
+    return payload;
+  } catch (error) {
+    throw error;
+  }
+}
+
+export function isJWTExpired(token: string): boolean {
+  try {
+    const payload = parseJWTPayload(token);
+    return payload.exp < Date.now() / MS_PER_SEC;
+  } catch (error) {
+    console.error(error);
+    return true;
+  }
+}
 
 export function clearToken() {
-  localStorage.clear();
+  localStorage.removeItem(TOKEN);
 }
 
 export function saveToken(token: string) {
-  const tokenObj = {
-    value: token,
-    expire: Date.now() + expireDays * 24 * 60 * 60 * 1000,
-  };
-  const tokenObjString = JSON.stringify(tokenObj);
-  localStorage.setItem(TOKEN, tokenObjString);
+  localStorage.setItem(TOKEN, token);
 }
 
 export function getToken() {
-  const tokenObjString = localStorage.getItem(TOKEN);
-  if (!tokenObjString) {
-    return null;
-  }
-  const tokenObj = JSON.parse(tokenObjString);
-  if (Date.now() > tokenObj.expire) {
+  let token = localStorage.getItem(TOKEN);
+  if (token !== null && isJWTExpired(token)) {
     clearToken();
-    return null;
+    token = null;
   }
-  return tokenObj.value;
+  return token;
 }
